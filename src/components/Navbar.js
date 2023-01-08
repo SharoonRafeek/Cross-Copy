37f4f78a377e72d84e0adedaf405d86180a46e25
Sharoon <sharoonrafeek@gmail.co>
2023-01-08 20:28:50 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <span className="text-3xl font-bold text-purple-600">Cross Copy</span>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
