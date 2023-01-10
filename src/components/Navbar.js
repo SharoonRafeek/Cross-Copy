fc44d3d9883725727ec187defe9671f3af9a3d13
Sharoon <sharoonrafeek@gmail.co>
2023-01-10 21:04:30 +0530

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,9 @@ const Navbar = () => {
   return (
     <>
       <header>
-        <nav className='p-8 flex flex-row text-purple-600 justify-between'>
-          <span className='text-xl font-bold lg:text-3xl md:text-2xl'>Cross Copy</span>
+        <nav className='p-8 flex flex-row text-white   justify-between'>
+          <span className='text-xl font-bold lg:text-3xl md:text-2xl'>CrossCopy.</span>
           <div className='space-x-2 lg:space-x-9 md:space-x-5'>
-            <span><a className='header-link' href="/">Home</a></span>
             <span><a className='header-link' href="/about">About</a></span>
             <span><a className='header-link' href="/contact">Contact</a></span>
             <span><a className='header-link' href="https://github.com/SharoonRafeek/Cross-Copy">Contribute</a></span>
