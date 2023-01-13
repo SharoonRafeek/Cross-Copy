e159e14c6ea94d3bc17ebcab47923c394802ac0b
Sharoon <sharoonrafeek@gmail.com>
2023-01-13 20:26:48 +0530

    brand styled.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Button = () => {
     return (
-        <div>
+        <div className='pt-5'>
             <form action="">
-                <button> Get Started </button>
+                <button className='bg-cyan-600'> <h4 className='px-3 pt-1 text-white text-xl font-bold'>Get Started</h4></button>
             </form>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <>
       <header>
-        <nav className='flex flex-row text-white   justify-between'>
-          <span className='lg:pl-32 md:pl-16 text-xl font-bold lg:text-3xl md:text-2xl'>CrossCopy.</span>
-          <div className='lg:pr-32 md:pr-16 space-x-2 lg:space-x-9 md:space-x-5'>
-            <span><a className='header-link' href="/about">About</a></span>
-            <span><a className='header-link' href="/contact">Contact</a></span>
-            <span><a className='header-link' href="https://github.com/SharoonRafeek/Cross-Copy">Contribute</a></span>
+        <nav className="flex flex-row text-white   justify-between">
+          <span className="lg:ml-32 md:ml-16 text-xl px-3 pt-1 font-bold lg:text-3xl md:text-2xl bg-cyan-600">
+            CrossCopy.
+          </span>
+          <div className="lg:pr-32 md:pr-16 space-x-2 lg:space-x-9 md:space-x-5">
+            <span>
+              <a className="header-link" href="/about">
+                About
+              </a>
+            </span>
+            <span>
+              <a className="header-link" href="/contact">
+                Contact
+              </a>
+            </span>
+            <span>
+              <a
+                className="header-link"
+                href="https://github.com/SharoonRafeek/Cross-Copy"
+              >
+                Contribute
+              </a>
+            </span>
           </div>
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
