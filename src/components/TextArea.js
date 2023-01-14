5d40eb0a09d4f5672be7d05e79618cdc040dc927
Sharoon <sharoonrafeek@gmail.com>
2023-01-14 15:23:00 +0530

    homepage redesign

---
+++ src/components/TextArea.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TextArea = () => {
+    return (
+        <div>
+            <form action="">
+
+            </form>
+        </div>
+    )
+}
+
+export default TextArea
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,6 @@
-import React from "react";
-import Button from "../components/Button";
 
 const Home = () => {
-  return <div className="lg:pl-32 md:pl-16">
-    <div className="home-title">
-      <h1>
-        Copy Across
-      </h1>
-      <h1>
-        Different
-      </h1>
-      <h1>
-        Devices.
-      </h1>
-    </div>
 
-    <Button />
-  </div>;
 };
 
 export default Home;
