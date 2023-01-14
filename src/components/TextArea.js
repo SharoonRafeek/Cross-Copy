5de4eaf533ec1eaffd260cfba06b9bb110ba8279
Sharoon <sharoonrafeek@gmail.com>
2023-01-14 23:56:19 +0530

    minor changes

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -15,9 +15,9 @@ const TextArea = () => {
         alert(`You have copied "${copyText}"`);
     }
     return (
-        <div>
+        <div className='w-auto lg:w-500'>
             <form>
-                <textarea onChange={handleCopyText} className='lg:w-500 w-full text-black bg-gray-100 ring-cyan-600 border-4 outline-none p-3 border-cyan-600' rows="12" placeholder='Paste your text here...'></textarea>
+                <textarea onChange={handleCopyText} className='w-full text-black bg-gray-100 ring-cyan-600 border-4 outline-none p-3 border-cyan-600' rows="12" placeholder='Paste your text here...'></textarea>
                 <div>
                     <button className='bg-cyan-600 hover:bg-cyan-500' onClick={copyToClipboard}> <h4 className='px-3 pt-1 text-white text-xl font-bold'>Copy</h4></button>
                 </div>
