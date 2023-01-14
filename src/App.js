cafe8a987e473d38220332aa315b8fbda5c74817
Sharoon <sharoonrafeek@gmail.com>
2023-01-14 22:06:54 +0530

    default route changed.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Randomstring from "randomstring";
 
 function App() {
+  const route = Randomstring.generate(4);
+
   return (
     <div>
       <Navbar />
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to={"/" + route} />} />
+          <Route path="/:route" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -19,7 +19,7 @@ const TextArea = () => {
             <form>
                 <textarea onChange={handleCopyText} className='lg:w-2/5 md:w-1/2 w-full text-black bg-gray-100 ring-cyan-600 border-4 outline-none p-3 border-cyan-600' rows="12" placeholder='Paste your text here...'></textarea>
                 <div>
-                    <button className='bg-cyan-600' onClick={copyToClipboard}> <h4 className='px-3 pt-1 text-white text-xl font-bold'>Copy</h4></button>
+                    <button className='bg-cyan-600 hover:bg-cyan-400' onClick={copyToClipboard}> <h4 className='px-3 pt-1 text-white text-xl font-bold'>Copy</h4></button>
                 </div>
             </form>
         </div>
