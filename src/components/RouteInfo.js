cedd1e0b0c64aa163c3739fce76aadcf5c832df1
Sharoon <sharoonrafeek@gmail.com>
2023-01-14 23:47:48 +0530

    qrcode added

--- src/components/RouteInfo.js
+++ src/components/RouteInfo.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import QRCode from "qrcode.react";
 
 const RouteInfo = () => {
     const location = useLocation();
+    const url = "www.crosscopy.com" + location.pathname;
     return (
-        <div className="text-xl text-center space-y-14">
+        <div className="text-xl text-center space-y-14 lg:pt-0 pt-10">
             <h3>
-                Go to <a className="bg-cyan-600">www.crosscopy.com{location.pathname}</a>
+                Go to <a className="bg-cyan-600">{url}</a>
             </h3>
-            <h3 className="">
+            <h3>
                 Or scan
             </h3>
+            <div className="flex justify-center">
+                <QRCode value={url} />
+            </div>
         </div>
     )
 };
