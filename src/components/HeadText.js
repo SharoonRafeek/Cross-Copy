f3ff3eafeeeae929c45788b0204cd998a6157564
SharoonRafeek <sharoonrafeek@gmail.com>
2023-05-28 16:54:13 +0530

    cleanup

--- src/components/HeadText.js
+++ src/components/HeadText.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const HeadText = () => {
-    return (
-        <div className='mt-12'>
-            <h1 className='text-4xl leading-normal text-center'>
-                Effortlessly transfer text across <br /> all your devices with <span className='bg-cyan-600 font-bold px-3 pt-1'>CrossCopy.</span>
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div className="mt-12 w-full flex justify-center">
+      <h1 className="text-4xl leading-normal lg:w-3/4 text-center">
+        Effortlessly transfer text across all your devices with &npsb;
+        <h1 className="bg-cyan-600 font-bold px-3 pt-1">CrossCopy.</h1>
+      </h1>
+    </div>
+  );
+};
