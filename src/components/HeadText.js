f3812a3a30b625105ae68d55767194961e8a28e6
SharoonRafeek <sharoonrafeek@gmail.com>
2023-05-28 16:58:14 +0530

    bug fixes

--- src/components/HeadText.js
+++ src/components/HeadText.js
@@ -4,8 +4,8 @@ export const HeadText = () => {
   return (
     <div className="mt-12 w-full flex justify-center">
       <h1 className="text-4xl leading-normal lg:w-3/4 text-center">
-        Effortlessly transfer text across all your devices with &npsb;
-        <h1 className="bg-cyan-600 font-bold px-3 pt-1">CrossCopy.</h1>
+        Effortlessly transfer text across all your devices with&nbsp;
+        <span className="bg-cyan-600 font-bold px-3 pt-1">CrossCopy.</span>
       </h1>
     </div>
   );
